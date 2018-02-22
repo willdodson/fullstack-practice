1510ad99465d12951e4c3ef2e59c80762df98b25
wsdodson <wsdodson@gmail.com>
2018-02-22 11:19:23 -0700

    Acc component

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import './Account/css'
 
 export default class Account extends Component {
     render() {
+        const {picture,name,email,amount,id} = this.props
+
         return (
-            <h1>Account</h1>
+            <div className = "account">
+                <h1>Account</h1>
+                <img src={picture} />
+                <div><span className="bold">Name</span>{name}</div>
+                <div><span className="bold">Email</span>{email}</div>
+                <div><span className="bold">Amount</span>{amount}</div>
+                <div><span className="bold">ID</span>{id}</div>
+            </div>
         );
     }
 }
